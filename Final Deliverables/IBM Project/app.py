5ebb6a33e3b6f278efd89b2ef85f2398706c9aed
guillotine0406 <97660026+guillotine0406@users.noreply.github.com>
2022-11-19 11:33:05 +0530

    Update app.py

--- Final Deliverables/IBM Project/app.py
+++ Final Deliverables/IBM Project/app.py
@@ -400,26 +400,6 @@ def apply_job():
     elif request.method == 'POST': msg="Please fill out the form."
     return render_template('apply.html')
 
-def sendgridmail(to_mail_id):
-    try:
-        sg = sendgrid.SendGridAPIClient(
-            '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')
-    # Change to your verified sender
-        from_email = Email("kencydanielfdo@gmail.com")
-        to_email = To(to_mail_id)  # Change to your recipient
-        subject = "Plasma Donation request "
-        htmlcontent = "Hi, A user has sent you a request for plasma donation. If you are willing to donate kindly contact them with this email id. Email: " + \
-            session['email']
-        content = Content("text/plain", htmlcontent)
-        mail = Mail(from_email, to_email, subject, content)
-    # Get a JSON-ready representation of the Mail object
-        mail_json = mail.get()
-    # Send an HTTP POST request to /mail/send
-        response = sg.client.mail.send.post(request_body=mail_json)
-        print(response.status_code)
-    except Exception as e:
-        print(e.message)
-
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
 
